5723586bb25174b8663ddd53ba002a8ee5ab431f
Amateur-God <68581288+Amateur-God@users.noreply.github.com>
2022-10-20 18:57:32 +0100

    Added ONI Mod to Repo

---
+++ releases/ONI_Recon_111_Essentials/mod.cpp
@@ -0,0 +1,7 @@
+name = "ONI Recon 111 Essentials";
+picture = "ONI_Recon_111.paa";
+hidePicture = "true";
+hideName = "false";
+actionName = "Discord";
+action = "https://discord.gg/UjmVpqrEqW";
+description = "Halo Custom Textures for Oni Recon 111 Unit";
