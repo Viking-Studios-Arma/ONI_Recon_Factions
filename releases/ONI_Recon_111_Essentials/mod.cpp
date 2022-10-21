d87394cd1cb0bc52530b1140ddb62a5255d5098a
Amateur-God <68581288+Amateur-God@users.noreply.github.com>
2022-10-21 22:41:37 +0100

    Name Change + New Textures

--- releases/ONI_Recon_111_Essentials/mod.cpp
+++
@@ -1,7 +0,0 @@
-name = "ONI Recon 111 Essentials";
-picture = "ONI_Recon_111.paa";
-hidePicture = "true";
-hideName = "false";
-actionName = "Discord";
-action = "https://discord.gg/UjmVpqrEqW";
-description = "Halo Custom Textures for Oni Recon 111 Unit";
