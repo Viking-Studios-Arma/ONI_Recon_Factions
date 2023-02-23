4c48475a46ccdaed1d0b83192793e4d9cd85e4c1
Viking Studios <68581288+Amateur-God@users.noreply.github.com>
2023-02-23 12:10:09 +0000

    Helicopter Pilot Fix

--- Addons/Factions/Helicopters/Helicopters.hpp
+++ Addons/Factions/Helicopters/Helicopters.hpp
@@ -3,10 +3,16 @@
     faction="ONI_Recon_Faction";
     editorSubcategory = "ONI_Recon_Faction_Helicopters";
     side=1;
-    displayName="UH144 Falcon";
+    crew="ONI_Recon_Essentials_Pilot";
+    displayName="UH144 Falcon (Armed)";
     hiddenSelectionsTextures[]={"optre_vehicles\falcon\data\falcon_hull_night_co.paa","optre_vehicles\falcon\data\falcon_hullextra_night_co.paa","optre_vehicles\falcon\data\falcon_wingtips_night_co.paa","optre_vehicles\falcon\data\falcon_skid_night_co.paa","","",""};
-    crew="ONI_Recon_Essentials_Heli_Pilot";
-    typicalCargo[]={"ONI_Recon_Essentials_Heli_Pilot"};
+		weapons[] = {"CMFlareLauncher","Laserdesignator_mounted"};
+		magazines[] = {"168Rnd_CMFlare_Chaff_Magazine","Laserbatteries"};
+		tf_range = 25000;
+		tf_isolatedAmount = 0.4;
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		enableRadio = 1;
   };
 
   class ONI_Recon_Essentials_UH144_Falcon_Unarmed: OPTRE_UNSC_falcon_unarmed
@@ -14,8 +20,12 @@
     faction="ONI_Recon_Faction";
     editorSubcategory = "ONI_Recon_Faction_Helicopters";
     side=1;
-    displayName="UH144 Falcon Unarmed";
+    displayName="UH144 Falcon (Unarmed)";
+    crew="ONI_Recon_Essentials_Pilot";
     hiddenSelectionsTextures[]={"optre_vehicles\falcon\data\falcon_hull_night_co.paa","optre_vehicles\falcon\data\falcon_hullextra_night_co.paa","optre_vehicles\falcon\data\falcon_wingtips_night_co.paa","optre_vehicles\falcon\data\falcon_skid_night_co.paa","","","",""};
-    crew="ONI_Recon_Essentials_Heli_Pilot";
-    typicalCargo[]={"ONI_Recon_Essentials_Heli_Pilot"};
+		tf_range = 25000;
+		tf_isolatedAmount = 0.4;
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		enableRadio = 1; 
   };
