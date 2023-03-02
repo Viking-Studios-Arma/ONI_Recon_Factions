fd45a498c3e7620dbfcd823dfeed5dd3cf1fd24f
Viking Studios <68581288+Amateur-God@users.noreply.github.com>
2023-03-02 11:43:29 +0000

    updated mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,7 +1,7 @@
-name = "ONI Recon";
+name = "ONI Recon - Factions and Equipment";
 picture = "ONI_Recon.paa";
 hidePicture = "true";
 hideName = "false";
 actionName = "Discord";
 action = "https://discord.gg/9pJTHHzaFC";
-description = "Halo Custom Textures for Oni Recon Unit";
+description = "Halo Custom Faction and equipment textures for non-canon ONI Recon Faction";
