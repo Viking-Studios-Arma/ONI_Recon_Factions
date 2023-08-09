ce3ef72ea1ee1365a7ea7d883f930bd0f6f7cdf0
Viking Studios <mrhowe95@gmail.com>
2023-08-09 02:14:40 +0100

    removed XtdGearInfo from backpacks due to bugs

--- Addons/Backpacks/config.cpp
+++ Addons/Backpacks/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches {
 		version = 1.0;
 		versionAr[] = {1,0,0,0};
 		versionStr = "v1.0.0";
-        units[] = {"ONI_Recon_Rucksack_Base","ONI_Recon_Rucksack_Base_Medic","ONI_Recon_Rucksack_Base_IC","ONI_Recon_Rucksack","ONI_Recon_Rucksack_Medic","ONI_Recon_Rucksack_IC"};
+        units[] = {"ONI_Recon_Backpack","ONI_Recon_Rucksack_Base","ONI_Recon_Rucksack_Base_Medic","ONI_Recon_Rucksack_Base_IC","ONI_Recon_Rucksack","ONI_Recon_Rucksack_Medic","ONI_Recon_Rucksack_IC"};
         weapons[] = {};
         requiredAddons[] = {"OPTRE_Core"};
     };
@@ -23,13 +23,15 @@ class cfgVehicles
     // Base Back pack, dont use in arsenal
     class ONI_Recon_Rucksack_Base: OPTRE_ILCS_Rucksack_Heavy
     {
-        scope = 1;
+        scope = 2;
         scopeArsenal = 1;
-        scopeCurator = 2;
+        scopeCurator = 1;
         isbackpack = 1;
         reversed = 1;
         mapSize = 2;
         vehicleClass = Backpacks;
+        hiddenSelections[] = {"camo1","AP_Heavy","biofoam"};
+        hiddenSelectionsTextures[] = {"OPTRE_Weapons\Backpacks\data\ruck_black_CO.paa"};
         allowedSlots[] = {901};
         displayName = "[ONI] Rucksacks Base";
         maximumLoad = 2000;
@@ -37,27 +39,31 @@ class cfgVehicles
     // Base Back pack for Medic dont use in arsenal
     class ONI_Recon_Rucksack_Base_Medic: OPTRE_ILCS_Rucksack_Medical
     {
-        scope = 1;
+        scope = 2;
         scopeArsenal = 1;
         //<-----This  
-        scopeCurator = 2;      
+        scopeCurator = 1;      
         isbackpack = 1;
         reversed = 1;
         mapSize = 2;
         vehicleClass = Backpacks;
+        hiddenSelections[] = {"camo1","camoBiofoam","AP_Heavy"};
+        hiddenSelectionsTextures[] = {"OPTRE_Weapons\Backpacks\data\ruck_black_CO.paa","optre_weapons\items\data\biofoam_co.paa"};
         displayName = "[ONI] Rucksacks Base Medic";
         maximumLoad = 2000;
     };
     // Base Back pack for IC dont use in arsenal
     class ONI_Recon_Rucksack_Base_IC: OPTRE_ANPRC_521_Black
     {
-        scope = 1;
+        scope = 2;
         scopeArsenal = 1;
-        scopeCurator = 2;
+        scopeCurator = 1;
         isbackpack = 1;
         reversed = 1;
         mapSize = 2;
         vehicleClass = Backpacks;
+        hiddenSelections[] = {"camo","camo2","B_Medic","B_Addons"};
+        hiddenSelectionsTextures[] = {"optre_unsc_units\army\data\soft_backpack_co.paa","optre_unsc_units\army\data\commopack_blk_co.paa"};
         allowedSlots[] = {901};
         displayName = "[ONI] Rucksacks Base IC";
         maximumLoad = 2000;
@@ -68,12 +74,14 @@ class cfgVehicles
         scope = 2;
         scopeArsenal = 2;
         scopeCurator = 2;
-        displayName = "[ONI] Backpack";
+        displayName = "[ONI] Rifleman";
         maximumLoad = 450;
+        /*
             class XtdGearInfo {
             model = "ONI_Recon_Backpack";
             camo = "A";
         };
+        */
     };
     // Medic BackPack
         class ONI_Recon_Rucksack_Medic: ONI_Recon_Rucksack_Base_Medic
@@ -81,12 +89,14 @@ class cfgVehicles
         scope = 2;
         scopeArsenal = 2;
         scopeCurator = 2;
-        displayName = "[ONI] Medic Backpack";
+        displayName = "[ONI] Medic";
         maximumLoad = 500;
+        /*
             class XtdGearInfo {
             model = "ONI_Recon_Backpack";
             camo = "B";
         };
+        */
     };
     /* 
     Did you know the %Chance that aliens are out there is higher than the %chance that they arent
@@ -98,16 +108,19 @@ class cfgVehicles
         scope = 2;
         scopeArsenal = 2;
         scopeCurator = 2;
-        displayName = "[ONI] IC Backpack";        
+        displayName = "[ONI] IC";        
         maximumLoad = 350;
+            /*
             class XtdGearInfo {
             model = "ONI_Recon_Backpack";
             camo = "C";
         };
+        */
     };
 };
 //IDK why im writing this stuff in here, no one is going to read it.
  /*42*/
+/* Has decided it does not want to work
 class XtdGearModels 
 {
     class cfgVehicles 
@@ -130,3 +143,4 @@ class XtdGearModels
         };
     };
 };
+*/
