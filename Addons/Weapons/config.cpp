ea037bf0908a6ca6d55887aa6a2e5bf5ed63203f
gumbekk <gumbekk13@gmail.com>
2023-08-09 16:36:06 -0400

    add baseWeapon

--- Addons/Weapons/config.cpp
+++ Addons/Weapons/config.cpp
@@ -6,8 +6,9 @@ class CfgPatches {
         Authors[] = {"Amateur-god", "gumbekk"};
         versionStr = "v1.0.0";
         units[] = {};
-        weapons[] = {"ONI_Weapon_Commando_Tan","ONI_Weapon_Commando_Snow","ONI_Weapon_Commando_Black","ONI_Weapon_Commando","ONI_Weapon_FC_T51J_Repeater","ONI_Weapon_FC_T51_Repeater","ONI_Weapon_FC_T51_Carbine","ONI_Weapon_FC_T50_SRS","ONI_Weapon_FC_T50_ConcussionRifle","ONI_Weapon_FC_T25J_Rifle","ONI_Weapon_FC_T25_Rifle","ONI_Weapon_SRS99D","ONI_Weapon_SRS99C","ONI_Weapon_SRM77_S2","ONI_Weapon_SRM77_S1","ONI_Weapon_MA5K","ONI_Weapon_MA5C","ONI_Weapon_MA5CGL","ONI_Weapon_MA5B","ONI_Weapon_MA5BGL","ONI_Weapon_MA32B","ONI_Weapon_MA32BGL","ONI_Weapon_MA37B","ONI_Weapon_MA37BGL","ONI_Weapon_MA5AGL","ONI_Weapon_MA5A","ONI_Weapon_MA37K","ONI_Weapon_MA37","ONI_Weapon_MA37GL","ONI_Weapon_MA32","ONI_Weapon_MA32GL","ONI_Weapon_M99A2S3","ONI_Weapon_M90A","ONI_Weapon_M73","ONI_Weapon_M58S","ONI_Weapon_M7","ONI_Weapon_M45E","ONI_Weapon_M45ATAC","ONI_Weapon_M45A","ONI_Weapon_M45","ONI_Weapon_M45TAC","ONI_Weapon_M393S_DMR","ONI_Weapon_M393_DMR","ONI_Weapon_M392_DMR","ONI_Weapon_M319n","ONI_Weapon_M319","ONI_Weapon_M295_BMR_Woodland","ONI_Weapon_M295_BMR_Snow","ONI_Weapon_M295_BMR_Desert","ONI_Weapon_M295_BMR","ONI_Weapon_M247","ONI_Weapon_FC_T33_FuelRod_Cannon_Guided","ONI_Weapon_FC_T33_FuelRod_Cannon","ONI_Weapon_M41_SSR","ONI_Weapon_FC_T25J_Rifle_Folded","ONI_Weapon_FC_T25_Rifle_Folded","ONI_Weapon_FC_Plasma_Pistol","ONI_Weapon_hgun_sas10_F","ONI_Weapon_hgun_comet_F","ONI_Weapon_M7_Folded","ONI_Weapon_M6G","ONI_Weapon_M6C","ONI_Weapon_M6B","ONI_Weapon_M319M","ONI_Weapon_M319M"};
-        requiredAddons[]={"OPTRE_Weapons", "OPTRE_Core"};
+        weapons[] = {"ONI_Weapon_M295_BMR"};
+        requiredAddons[]={"OPTRE_Weapons"};
     };
 };
+
 #include "hpp\CfgWeapons.hpp"
