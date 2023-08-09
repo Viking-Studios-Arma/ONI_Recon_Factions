7796a89966f2fd0c34e0cb41d994445aa72d2e57
gumbekk <gumbekk13@gmail.com>
2023-08-09 15:13:32 -0400

    only Desert BMR shows up unsure why

---
+++ Addons/Weapons/hpp/CfgWeapons.hpp
@@ -0,0 +1,198 @@
+class CfgWeapons {
+	#include "parents.hpp"
+	class ONI_Weapon_Commando_Tan: OPTRE_Commando_Tan{
+		displayName = "[ONI] Commando Tan";
+	};
+	class ONI_Weapon_Commando_Snow: OPTRE_Commando_Snow{
+		displayName = "[ONI] Commando Snow";
+	};
+	class ONI_Weapon_Commando_Black: OPTRE_Commando_Black{
+		displayName = "[ONI] Commando Black";
+	};
+	class ONI_Weapon_Commando: OPTRE_Commando{
+		displayName = "[ONI] Commando";
+	};
+	class ONI_Weapon_FC_T51J_Repeater: OPTRE_FC_T51J_Repeater{
+		displayName = "[ONI] FC T51J Repeater";
+	};
+	class ONI_Weapon_FC_T51_Repeater: OPTRE_FC_T51_Repeater{
+		displayName = "[ONI] FC T51 Repeater";
+	};
+	class ONI_Weapon_FC_T51_Carbine: OPTRE_FC_T51_Carbine{
+		displayName = "[ONI] FC T51 Carbine";
+	};
+	class ONI_Weapon_FC_T50_SRS: OPTRE_FC_T50_SRS{
+		displayName = "[ONI] FC T50 SRS";
+	};
+	class ONI_Weapon_FC_T50_ConcussionRifle: OPTRE_FC_T50_ConcussionRifle{
+		displayName = "[ONI] FC T50 ConcussionRifle";
+	};
+	class ONI_Weapon_FC_T25J_Rifle: OPTRE_FC_T25J_Rifle{
+		displayName = "[ONI] FC T25J Rifle";
+	};
+	class ONI_Weapon_FC_T25_Rifle: OPTRE_FC_T25_Rifle{
+		displayName = "[ONI] FC T25 Rifle";
+	};
+	class ONI_Weapon_SRS99D: OPTRE_SRS99D{
+		displayName = "[ONI] SRS99D";
+	};
+	class ONI_Weapon_SRS99C: OPTRE_SRS99C{
+		displayName = "[ONI] SRS99C";
+	};
+	class ONI_Weapon_SRM77_S2: OPTRE_SRM77_S2{
+		displayName = "[ONI] SRM77 S2";
+	};
+	class ONI_Weapon_SRM77_S1: OPTRE_SRM77_S1{
+		displayName = "[ONI] SRM77 S1";
+	};
+	class ONI_Weapon_MA5K: OPTRE_MA5K{
+		displayName = "[ONI] MA5K";
+	};
+	class ONI_Weapon_MA5C: OPTRE_MA5C{
+		displayName = "[ONI] MA5C";
+	};
+	class ONI_Weapon_MA5CGL: OPTRE_MA5CGL{
+		displayName = "[ONI] MA5CGL";
+	};
+	class ONI_Weapon_MA5B: OPTRE_MA5B{
+		displayName = "[ONI] MA5B";
+	};
+	class ONI_Weapon_MA5BGL: OPTRE_MA5BGL{
+		displayName = "[ONI] MA5BGL";
+	};
+	class ONI_Weapon_MA32B: OPTRE_MA32B{
+		displayName = "[ONI] MA32B";
+	};
+	class ONI_Weapon_MA32BGL: OPTRE_MA32BGL{
+		displayName = "[ONI] MA32BGL";
+	};
+	class ONI_Weapon_MA37B: OPTRE_MA37B{
+		displayName = "[ONI] MA37B";
+	};
+	class ONI_Weapon_MA37BGL: OPTRE_MA37BGL{
+		displayName = "[ONI] MA37BGL";
+	};
+	class ONI_Weapon_MA5AGL: OPTRE_MA5AGL{
+		displayName = "[ONI] MA5AGL";
+	};
+	class ONI_Weapon_MA5A: OPTRE_MA5A{
+		displayName = "[ONI] MA5A";
+	};
+	class ONI_Weapon_MA37K: OPTRE_MA37K{
+		displayName = "[ONI] MA37K";
+	};
+	class ONI_Weapon_MA37: OPTRE_MA37{
+		displayName = "[ONI] MA37";
+	};
+	class ONI_Weapon_MA37GL: OPTRE_MA37GL{
+		displayName = "[ONI] MA37GL";
+	};
+	class ONI_Weapon_MA32: OPTRE_MA32{
+		displayName = "[ONI] MA32";
+	};
+	class ONI_Weapon_MA32GL: OPTRE_MA32GL{
+		displayName = "[ONI] MA32GL";
+	};
+	class ONI_Weapon_M99A2S3: OPTRE_M99A2S3{
+		displayName = "[ONI] M99A2S3";
+	};
+	class ONI_Weapon_M90A: OPTRE_M90A{
+		displayName = "[ONI] M90A";
+	};
+	class ONI_Weapon_M73: OPTRE_M73{
+		displayName = "[ONI] M73";
+	};
+	class ONI_Weapon_M58S: OPTRE_M58S{
+		displayName = "[ONI] M58S";
+	};
+	class ONI_Weapon_M7: OPTRE_M7{
+		displayName = "[ONI] M7";
+	};
+	class ONI_Weapon_M45E: OPTRE_M45E{
+		displayName = "[ONI] M45E";
+	};
+	class ONI_Weapon_M45ATAC: OPTRE_M45ATAC{
+		displayName = "[ONI] M45ATAC";
+	};
+	class ONI_Weapon_M45A: OPTRE_M45A{
+		displayName = "[ONI] M45A";
+	};
+	class ONI_Weapon_M45: OPTRE_M45{
+		displayName = "[ONI] M45";
+	};
+	class ONI_Weapon_M45TAC: OPTRE_M45TAC{
+		displayName = "[ONI] M45TAC";
+	};
+	class ONI_Weapon_M393S_DMR: OPTRE_M393S_DMR{
+		displayName = "[ONI] M393S DMR";
+	};
+	class ONI_Weapon_M393_DMR: OPTRE_M393_DMR{
+		displayName = "[ONI] M393 DMR";
+	};
+	class ONI_Weapon_M392_DMR: OPTRE_M392_DMR{
+		displayName = "[ONI] M392 DMR";
+	};
+	class ONI_Weapon_M319n: OPTRE_M319n{
+		displayName = "[ONI] M319n";
+	};
+	class ONI_Weapon_M319: OPTRE_M319{
+		displayName = "[ONI] M319";
+	};
+	class ONI_Weapon_M295_BMR_Woodland: OPTRE_M295_BMR_Woodland{
+		displayName = "[ONI] M295 BMR Woodland";
+	};
+	class ONI_Weapon_M295_BMR_Snow: OPTRE_M295_BMR_Snow{
+		displayName = "[ONI] M295 BMR Snow";
+	};
+	class ONI_Weapon_M295_BMR_Desert: OPTRE_M295_BMR_Desert{
+		displayName = "[ONI] M295 BMR Desert";
+	};
+	class ONI_Weapon_M295_BMR: OPTRE_M295_BMR{
+		displayName = "[ONI] M295 BMR";
+	};
+	class ONI_Weapon_M247: OPTRE_M247{
+		displayName = "[ONI] M247";
+	};
+	class ONI_Weapon_FC_T33_FuelRod_Cannon_Guided: OPTRE_FC_T33_FuelRod_Cannon_Guided{
+		displayName = "[ONI] FC T33 FuelRod Cannon Guided";
+	};
+	class ONI_Weapon_FC_T33_FuelRod_Cannon: OPTRE_FC_T33_FuelRod_Cannon{
+		displayName = "[ONI] FC T33 FuelRod Cannon";
+	};
+	class ONI_Weapon_M41_SSR: OPTRE_M41_SSR{
+		displayName = "[ONI] M41 SSR";
+	};
+	class ONI_Weapon_FC_T25J_Rifle_Folded: OPTRE_FC_T25J_Rifle_Folded{
+		displayName = "[ONI] FC T25J Rifle Folded";
+	};
+	class ONI_Weapon_FC_T25_Rifle_Folded: OPTRE_FC_T25_Rifle_Folded{
+		displayName = "[ONI] FC T25 Rifle Folded";
+	};
+	class ONI_Weapon_FC_Plasma_Pistol: OPTRE_FC_Plasma_Pistol{
+		displayName = "[ONI] FC Plasma Pistol";
+	};
+	class ONI_Weapon_hgun_sas10_F: optre_hgun_sas10_F{
+		displayName = "[ONI] hgun sas10 F";
+	};
+	class ONI_Weapon_hgun_comet_F: optre_hgun_comet_F{
+		displayName = "[ONI] hgun comet F";
+	};
+	class ONI_Weapon_M7_Folded: OPTRE_M7_Folded{
+		displayName = "[ONI] M7 Folded";
+	};
+	class ONI_Weapon_M6G: OPTRE_M6G{
+		displayName = "[ONI] M6G";
+	};
+	class ONI_Weapon_M6C: OPTRE_M6C{
+		displayName = "[ONI] M6C";
+	};
+	class ONI_Weapon_M6B: OPTRE_M6B{
+		displayName = "[ONI] M6B";
+	};
+	class ONI_Weapon_M319M: OPTRE_M319M{
+		displayName = "[ONI] M319M";
+	};
+	class ONI_Weapon_M319S: OPTRE_M319S{
+		displayName = "[ONI] M319S";
+	};
+};
