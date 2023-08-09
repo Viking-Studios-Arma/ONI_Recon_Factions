4b6c80cdbfffc471fd2855e688c693794f783b95
Viking Studios <mrhowe95@gmail.com>
2023-08-09 03:44:32 +0100

    forgot to commit these

--- Addons/Factions/Ground_Support/IFV.hpp
+++ Addons/Factions/Ground_Support/IFV.hpp
@@ -7,7 +7,7 @@
     displayName="M412 Bison IFV";
     hiddenSelectionsTextures[]={"optre_vehicles\bison\data\bison_body_blk_co.paa","optre_vehicles\bison\data\bison_misc_blk_co.paa","optre_vehicles\bison\data\bison_wheelscannon_blk_co.paa","optre_vehicles\bison\data\bison_seats_co.paa","optre_vehicles\bison\data\bison_interior_co.paa","optre_vehicles\bison\data\bison_mfcdscreens_co.paa","optre_vehicles\bison\data\bison_90mm_blk_co.paa"};
     crew="ONI_Recon_SF_Rifleman";
-    typicalCargo[]={"ONI_Recon_SF_Rifleman"};
+    typicalCargo={"ONI_Recon_SF_Rifleman"};
   };
 
   class ONI_Recon_M413_Bison_MGS: OPTRE_M413_MGS_UNSC
@@ -18,5 +18,5 @@
     displayName="M413 Bison MGS";
     hiddenSelectionsTextures[]={"optre_vehicles\bison\data\bison_body_blk_co.paa","optre_vehicles\bison\data\bison_misc_blk_co.paa","optre_vehicles\bison\data\bison_wheelscannon_blk_co.paa","optre_vehicles\bison\data\bison_seats_co.paa","optre_vehicles\bison\data\bison_interior_co.paa","optre_vehicles\bison\data\bison_mfcdscreens_co.paa","optre_vehicles\bison\data\bison_90mm_blk_co.paa"};
     crew="ONI_Recon_SF_Rifleman";
-    typicalCargo[]={"ONI_Recon_SF_Rifleman"};
+    typicalCargo={"ONI_Recon_SF_Rifleman"};
   };
--- Addons/Factions/Ground_Support/tanks.hpp
+++ Addons/Factions/Ground_Support/tanks.hpp
@@ -6,7 +6,7 @@
     displayName="M808S Lockdown Scorpion MBT";
     hiddenSelectionsTextures[]={"a3\armor_f_gamma\mbt_01\data\mbt_01_body_co.paa","a3\armor_f_gamma\mbt_01\data\mbt_01_tow_co.paa","a3\armor_f\data\camonet_nato_desert_co.paa"};
     crew="ONI_Recon_Armour_Crew";
-    typicalCargo[]={"ONI_Recon_Armour_Crew"};
+    typicalCargo={"ONI_Recon_Armour_Crew"};
   };
 
   class ONI_Recon_M808B2_Sun_Devil: OPTRE_M808B2
@@ -17,7 +17,7 @@
     displayName="M808B2 Sun Devil";
     hiddenSelectionsTextures[]={"optre_vehicles\m808b2\data\lckdw\body_lckdw_lopo_co.paa","optre_vehicles\m808b2\data\lckdw\cing_dv_lckdw_co.paa","optre_vehicles\m808b2\data\lckdw\cing_rt_lckdw_co.paa","optre_vehicles\m808b2\data\lckdw\turretbase_lckdw_lopo_co.paa","optre_vehicles\m808b2\data\night\sun_devil_head_lopo_co.paa","optre_vehicles\m808b2\data\night\sun_devil_gun_lopo_co.paa"};
     crew="ONI_Recon_Armour_Crew";
-    typicalCargo[]={"ONI_Recon_Armour_Crew"};
+    typicalCargo={"ONI_Recon_Armour_Crew"};
   };
 
   class ONI_Recon_M808S_Scorpion_MBT: OPTRE_M808S //Shall i make an anime waifu version?
@@ -28,7 +28,7 @@
     displayName="M808S Scorpion MBT";
     hiddenSelectionsTextures[]={"optre_vehicles\m808b2\data\lckdw\body_lckdw_lopo_co.paa","optre_vehicles\m808b2\data\lckdw\cing_dv_lckdw_co.paa","optre_vehicles\m808b2\data\lckdw\cing_rt_lckdw_co.paa","optre_vehicles\m808b2\data\lckdw\turretbase_lckdw_lopo_co.paa","optre_vehicles\m808b2\data\lckdw\turrethead_lckdw_lopo_co.paa","optre_vehicles\m808b2\data\night\turretguns_lopo_co.paa","optre_vehicles\m808b2\data\lckdw\ghammer_lckdw_lopo_co.paa"};
     crew="ONI_Recon_Armour_Crew";
-    typicalCargo[]={"ONI_Recon_Armour_Crew"};
+    typicalCargo={"ONI_Recon_Armour_Crew"};
   };
 
   class ONI_Recon_M808BM_Scorpion_MBT: OPTRE_M808BM_UNSC
@@ -39,7 +39,7 @@
     displayName="M808BM Scorpion MBT";
     hiddenSelectionsTextures[]={"optre_vehicles\scorpion\data\texture\camo\scorp_base_black_co.paa","optre_vehicles\scorpion\data\texture\camo\scorp_tur_black_co.paa","optre_vehicles\scorpion\data\texture\camo\det_3_black_co.paa","optre_vehicles\scorpion\data\texture\scorp_decals_ca.paa","optre_vehicles\scorpion\data\texture\scorp_net_wood_ca.paa","optre_vehicles\scorpion\data\texture\scorp_net_wood_ca.paa","optre_vehicles\scorpion\data\texture\camo\mine_roller_black_co.paa","optre_vehicles\scorpion\data\texture\camo\scorp_mg_black_co.paa"};
     crew="ONI_Recon_Armour_Crew";
-    typicalCargo[]={"ONI_Recon_Armour_Crew"};
+    typicalCargo={"ONI_Recon_Armour_Crew"};
   };
 
   class ONI_Recon_M808B_Scorpion_MBT: OPTRE_M808B_UNSC
@@ -50,5 +50,5 @@
     displayName="M808B Scorpion MBT";
     hiddenSelectionsTextures[]={"optre_vehicles\scorpion\data\texture\camo\scorp_base_black_co.paa","optre_vehicles\scorpion\data\texture\camo\scorp_tur_black_co.paa","optre_vehicles\scorpion\data\texture\camo\det_3_black_co.paa","optre_vehicles\scorpion\data\texture\scorp_decals_ca.paa","optre_vehicles\scorpion\data\texture\scorp_net_wood_ca.paa","optre_vehicles\scorpion\data\texture\scorp_net_wood_ca.paa","optre_vehicles\scorpion\data\texture\camo\mine_roller_black_co.paa"};
     crew="ONI_Recon_Armour_Crew";
-    typicalCargo[]={"ONI_Recon_Armour_Crew"};
+    typicalCargo={"ONI_Recon_Armour_Crew"};
   };
