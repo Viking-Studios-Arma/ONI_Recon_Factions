a8c071d6e9b224bd1638fb6bb75ff9aa4651a65c
gumbekk <gumbekk11771@gmail.com>
2023-08-18 04:18:17 -0400

    add PAAs and fix pathing

--- Addons/Armour/hpp/CfgWeapons.hpp
+++ Addons/Armour/hpp/CfgWeapons.hpp
@@ -139,6 +139,7 @@ class CfgWeapons
     class ONI_Recon_Armour_SF_gumbekk: ONI_Recon_Armour_SF_Basic
     {
         hiddenSelections[]={"camo", "camo2", "camo3", "camo4", "camo5","A_Ghillie","A_KneesMarLeft","A_KneesMarRight","AS_LargeLeft","AS_LargeRight","AS_MediumLeft","AS_MediumRight","AS_ODSTCQBLeft","AS_ODSTCQBRight","AS_ODSTRight","AS_ODSTSniperLeft","AS_SmallLeft", "AS_SmallRight","AP_GL","AP_Knife","AP_MGThigh","AP_AR","AP_BR","AP_Pack","AP_Pistol","AP_Rounds","AP_SG", "AP_SMG", "AP_Sniper","AP_Thigh","AP_Frag","AP_Smoke","APO_AR","APO_BR","APO_Knife","APO_SMG","APO_Sniper","CustomKit_Scorch"};
+        hiddenSelectionsTextures[] = {"\x\VS_ONI\Armour\data\ONI_Vest_co.paa","\x\VS_ONI\Armour\data\Armour_ONI_co.paa", "\x\VS_ONI\Armour\data\ONI_Legs_gumbekk_co.paa","\x\VS_ONI\Armour\data\ghillie_woodland_co.paa", "\x\VS_ONI\Armour\data\ONI_Armour_gumbekk_co.paa"};
         displayName = "[ONI] Special Forces Armour Gumbekk"; // It's 5AM help me with signing keys // Amateur-god (Carmichael) got Gumbekks GPG key verification working
         class ItemInfo: ItemInfo 
         {
