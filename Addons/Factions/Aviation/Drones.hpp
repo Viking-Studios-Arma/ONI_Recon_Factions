718eb5e73182efc2ac6ac503adbcada1ac64a282
gumbekk <gumbekk11771@gmail.com>
2023-08-29 07:37:47 -0400

    fixed crew

--- Addons/Factions/Aviation/Drones.hpp
+++ Addons/Factions/Aviation/Drones.hpp
@@ -37,8 +37,8 @@
     side=1;
     displayName="MQ221";
     hiddenSelectionsTextures[]={"v_fz_air_vehicles\data\dragonfly\a_mq221_blk_co.paa","v_fz_air_vehicles\data\dragonfly\a_mq221_blk_a_co.paa",""};
-    crew="B_AI_F";
-    typicalCargo[]={"B_AI_F"};
+    crew="B_UAV_AI";
+    typicalCargo[]={"B_UAV_AI"};
   };
   class ONI_Recon_MQ94: TCF_UAV_01_F
   {
@@ -47,8 +47,8 @@
     side=1;
     displayName="MQ94";
     hiddenSelectionsTextures[]={};
-    crew="B_AI_F";
-    typicalCargo[]={"B_AI_F"};
+    crew="B_UAV_AI";
+    typicalCargo[]={"B_UAV_AI"};
   };
   class ONI_Recon_Reaper: B_UAV_02_dynamicLoadout_F
   {
@@ -57,6 +57,6 @@
     side=1;
     displayName="Reaper";
     hiddenSelectionsTextures[]={"a3\drones_f\air_f_gamma\uav_02\data\uav_02_co.paa"};
-    crew="B_AI_F";
-    typicalCargo[]={"B_AI_F"};
+    crew="B_UAV_AI";
+    typicalCargo[]={"B_UAV_AI"};
   };
