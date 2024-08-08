2a6c6b1cd5e9731110fa5ebae7ffd2496c3a29d2
Viking Studios <mrhowe95@gmail.com>
2024-08-09 00:10:44 +0100

    revert broken textures to parent class textures untill 3.0.0 release

--- Addons/Factions/Aviation/Helicopters.hpp
+++ Addons/Factions/Aviation/Helicopters.hpp
@@ -5,9 +5,6 @@
     side=1;
     crew="ONI_Recon_Pilot";
     displayName="UH144 Falcon (Armed)";
-    hiddenSelectionsTextures[]={"OPTRE_Vehicles_Air\Falcon\data\black\Black_Falcon_Main_co.paa","OPTRE_Vehicles_Air\Falcon\data\black\Black_Falcon_Attachments_co.paa","OPTRE_Vehicles_Air\Falcon\data\black\Black_Falcon_Interior_co.paa"};
-		weapons[] = {"CMFlareLauncher","Laserdesignator_mounted"};
-		magazines[] = {"168Rnd_CMFlare_Chaff_Magazine","Laserbatteries"};
 		tf_range = 25000;
 		tf_isolatedAmount = 0.4;
 		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
@@ -22,12 +19,11 @@
     side=1;
     displayName="UH144 Falcon (Unarmed)";
     crew="ONI_Recon_Pilot";
-    hiddenSelectionsTextures[]={"OPTRE_Vehicles_Air\Falcon\data\black\Black_Falcon_Main_co.paa","OPTRE_Vehicles_Air\Falcon\data\black\Black_Falcon_Attachments_co.paa","OPTRE_Vehicles_Air\Falcon\data\black\Black_Falcon_Interior_co.paa"};
 		tf_range = 25000;
 		tf_isolatedAmount = 0.4;
 		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
 		tf_hasLRradio = 1;
-		enableRadio = 1; 
+		enableRadio = 1;
   };
   class ONI_Recon_Pelican_Unarmed: OPTRE_Pelican_unarmed_SOCOM
   {
@@ -35,14 +31,13 @@
     editorSubcategory = "EdSubcat_Helicopters";
     side=1;
     displayName="Pelican (Unarmed)";
-    hiddenSelectionsTextures[]={"optre_vehicles_air\pelican\data\pelicanexterior_black_co.paa","","","","",""};
     crew="ONI_Recon_Pilot";
     typicalCargo[]={"ONI_Recon_Pilot"};
     tf_range = 25000;
 		tf_isolatedAmount = 0.4;
 		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
 		tf_hasLRradio = 1;
-		enableRadio = 1; 
+		enableRadio = 1;
   };
   class ONI_Recon_Pelican_Armed: OPTRE_Pelican_armed_SOCOM
   {
@@ -50,14 +45,13 @@
     editorSubcategory = "EdSubcat_Helicopters";
     side=1;
     displayName="Pelican (Armed)";
-    hiddenSelectionsTextures[]={"optre_vehicles_air\pelican\data\pelicanexterior_black_co.paa","","","",""};
     crew="ONI_Recon_Pilot";
     typicalCargo[]={"ONI_Recon_Pilot"};
 		tf_range = 25000;
 		tf_isolatedAmount = 0.4;
 		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
 		tf_hasLRradio = 1;
-		enableRadio = 1; 
+		enableRadio = 1;
   };
   class ONI_Recon_UH144S_Falcon: OPTRE_UNSC_falcon_s
   {
@@ -65,14 +59,13 @@
     editorSubcategory = "EdSubcat_Helicopters";
     side=1;
     displayName="UH144S Falcon Unarmed (Door Gunner)";
-    hiddenSelectionsTextures[]={"optre_vehicles_air\falcon\data\black\black_falcon_main_co.paa","optre_vehicles_air\falcon\data\black\black_falcon_attachments_co.paa","optre_vehicles_air\falcon\data\black\black_falcon_interior_co.paa",""};
     crew="ONI_Recon_Pilot";
     typicalCargo[]={"ONI_Recon_Pilot"};
     tf_range = 25000;
 		tf_isolatedAmount = 0.4;
 		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
 		tf_hasLRradio = 1;
-		enableRadio = 1; 
+		enableRadio = 1;
   };
   class ONI_Recon_UH144S_Falcon_Armed: OPTRE_UNSC_falcon_armed_s
   {
@@ -80,18 +73,18 @@
     editorSubcategory = "EdSubcat_Helicopters";
     side=1;
     displayName="UH144S Falcon (Door Gunner)";
-    hiddenSelectionsTextures[]={"optre_vehicles_air\falcon\data\black\black_falcon_main_co.paa","optre_vehicles_air\falcon\data\black\black_falcon_attachments_co.paa","optre_vehicles_air\falcon\data\black\black_falcon_interior_co.paa"};
     crew="ONI_Recon_Pilot";
     typicalCargo[]={"ONI_Recon_Pilot"};
     tf_range = 25000;
 		tf_isolatedAmount = 0.4;
 		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
 		tf_hasLRradio = 1;
-		enableRadio = 1; 
+		enableRadio = 1;
   };
   class ONI_Recon_EV41: TCF_UNSC_Nightingale
   {
     faction="ONI_Recon_Faction";
+    editorCategory = "ONI_Recon_Faction";
     editorSubcategory = "EdSubcat_Helicopters";
     side=1;
     displayName="EV41";
@@ -102,5 +95,5 @@
 		tf_isolatedAmount = 0.4;
 		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
 		tf_hasLRradio = 1;
-		enableRadio = 1; 
+		enableRadio = 1;
   };
--- Addons/Factions/Ground_Support/Artillery.hpp
+++ Addons/Factions/Ground_Support/Artillery.hpp
@@ -5,7 +5,6 @@
     side=1;
     scope = 2;
     displayName="M875 Scorpion Self Propelled Howitzer";
-    hiddenSelectionsTextures[]={"optre_vehicles\scorpion\data\texture\camo\scorp_base_black_co.paa","optre_vehicles\scorpion\data\texture\camo\scorp_art_black_co.paa","optre_vehicles\scorpion\data\texture\scorp_net_wood_ca.paa","optre_vehicles\scorpion\data\texture\scorp_art_camo_ca.paa","optre_vehicles\scorpion\data\texture\scorp_decals_ca.paa","optre_vehicles\scorpion\data\texture\camo\scorp_mg_black_co.paa","optre_vehicles\scorpion\data\texture\det_1_lopo_co.paa","optre_vehicles\scorpion\data\texture\camo\det_3_black_co.paa"};
     crew="ONI_Recon_Armour_Crew";
     typicalCargo[]={"ONI_Recon_Armour_Crew"};
   };
